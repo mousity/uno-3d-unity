c360c5310e895437a4a800faf03f45ceebe8c897
Samr Mouna <samr.mouna@gmail.com>
2025-07-17 02:49:44 -0400

    Added primitive card spacing function. Still needs adjusting

--- Assets/Scripts/ArcCards.cs
+++ Assets/Scripts/ArcCards.cs
@@ -1,12 +1,14 @@
+using System;
 using UnityEngine;
 
 public class ArcCards : MonoBehaviour
 {
     public float handWidth;
+    public Transform leftAnchor;
+    public Transform rightAnchor;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        handWidth = 4;
         SpaceCards();
     }
 
@@ -21,6 +23,18 @@ public class ArcCards : MonoBehaviour
         int cardCount = transform.childCount;
         if (cardCount == 0) return;
 
+        for (int i = 0; i < cardCount; i++)
+        {
+            float t = (float)i / cardCount;
+
+            Vector3 pos = Vector3.Lerp(leftAnchor.position, rightAnchor.position, t);
+            pos = new Vector3(pos.x, leftAnchor.position.y, leftAnchor.position.z);
+            Debug.Log("hi");
+            Debug.Log(pos.ToString());
+
+            transform.GetChild(i).localPosition = pos;
+        }
+        
 
     }
 }
