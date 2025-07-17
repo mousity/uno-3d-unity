3c0f334f426f5eb46d9c00ccc5f917b5d5ea4665
Samr Mouna <samr.mouna@gmail.com>
2025-07-16 20:51:34 -0400

    Added short GameState list

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,10 +3,20 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public CardSpawner spawner;
+    public enum GameState
+    {
+        StartGame,
+        PlayerTurn,
+        EnemyTurn,
+        Win,
+        Lose
+    }
+    public GameState currentState;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        currentState = GameState.StartGame;
         spawner.DrawRandomCard();
     }
 
