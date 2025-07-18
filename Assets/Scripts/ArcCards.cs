cfe49878556a95275d19bca2905152ae321103a3
Samr Mouna <samr.mouna@gmail.com>
2025-07-17 21:35:23 -0400

    Added comments about Mathf.Lerp function for clarity

--- Assets/Scripts/ArcCards.cs
+++ Assets/Scripts/ArcCards.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 public class ArcCards : MonoBehaviour
 {
     public float handWidth;
-    public Transform leftAnchor;
-    public Transform rightAnchor;
+    public Transform leftAnchor; // Left hand anchor
+    public Transform rightAnchor; // Right hand anchor
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,24 +15,24 @@ public class ArcCards : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        SpaceCards();
     }
 
     void SpaceCards()
     {
-        int cardCount = transform.childCount;
-        if (cardCount == 0) return;
+        int cardCount = transform.childCount; // Get the amount of children in the player's hand (cards)
+        if (cardCount == 0) return; // No need to run this if we have no cards
 
-        for (int i = 0; i < cardCount; i++)
+        for (int i = 0; i < cardCount; i++) // For every card
         {
-            float t = (float)i / cardCount;
+            float t = (float)i / Mathf.Max(1, cardCount - 1); // The 't' in lerp is equal to i/cardcount - 1
 
-            Vector3 pos = Vector3.Lerp(leftAnchor.position, rightAnchor.position, t);
-            pos = new Vector3(pos.x, leftAnchor.position.y, leftAnchor.position.z);
+            Vector3 pos = Vector3.Lerp(leftAnchor.position, rightAnchor.position, t); // Get position between anchorpoints
+            pos = new Vector3(pos.x, leftAnchor.position.y, leftAnchor.position.z); // Ensure the Y and Z axis are the same for all cards
             Debug.Log("hi");
             Debug.Log(pos.ToString());
 
-            transform.GetChild(i).localPosition = pos;
+            transform.GetChild(i).localPosition = pos; // Locally position the card within the playerhand object
         }
         
 
