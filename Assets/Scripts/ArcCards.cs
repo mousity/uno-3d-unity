1e54951876b2efb94142c6f10496c0c0dcaca902
Samr Mouna <samr.mouna@gmail.com>
2025-07-17 23:14:02 -0400

    Creates dynamically growing and shrinking hand according to how many cards the player is holding

--- Assets/Scripts/ArcCards.cs
+++ Assets/Scripts/ArcCards.cs
@@ -1,23 +1,30 @@
 using System;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class ArcCards : MonoBehaviour
 {
     public float handWidth;
     public Transform leftAnchor; // Left hand anchor
-    public Transform rightAnchor; // Right hand anchor
-    public float zOffset;
+    public Transform rightAnchor; // Right hand anchor 0.55
+    public float anchorOffset; // Anchor offset when the hand grows or shrinks
+    public float zOffset; // z offset to let cards position behind one another
+    private Vector3 originalLAnchorPos; // Original position of the left anchor
+    private Vector3 originalRAnchorPos; // Original position of the right anchor
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        SpaceCards();
-        zOffset = 0.01f;
+
+        zOffset = 0.01f; // Set z offset
+        anchorOffset = 0.15f; // Set anchor offset
+        originalLAnchorPos = leftAnchor.position; // Set original left and right anchor positions
+        originalRAnchorPos = rightAnchor.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        SpaceCards();
+        SpaceCards(); // Space cards dynamically
     }
 
     void SpaceCards()
@@ -25,19 +32,32 @@ public class ArcCards : MonoBehaviour
         int cardCount = transform.childCount; // Get the amount of children in the player's hand (cards)
         if (cardCount == 0) return; // No need to run this if we have no cards
 
+        OffsetAnchors(cardCount); // Function to offset anchors according to card count
+
         for (int i = 0; i < cardCount; i++) // For every card
         {
             float t = (float)i / Mathf.Max(1, cardCount - 1); // The 't' in lerp is equal to i/cardcount - 1
 
             Vector3 pos = Vector3.Lerp(leftAnchor.position, rightAnchor.position, t); // Get position between anchorpoints
-            pos = new Vector3(pos.x, leftAnchor.position.y, leftAnchor.position.z + zOffset); // Ensure the Y and Z axis are the same for all cards
-            Debug.Log("hi");
-            Debug.Log(pos.ToString());
-            zOffset += 0.01f;
+            pos = new Vector3(pos.x, leftAnchor.position.y, leftAnchor.position.z); // Ensure the Y and Z axis are the same for all cards
+            pos.z += zOffset; // Modify the z position to be behind the previous card
+            zOffset += 0.01f; // Increase z offset for the next card
 
             transform.GetChild(i).localPosition = pos; // Locally position the card within the playerhand object
         }
 
-        zOffset = 0.01f;
+        zOffset = 0.01f; // Reset z offset so cards are not eternally going behind one another
+    }
+
+    void OffsetAnchors(int cardCount)
+    {
+        Vector3 newLPos = originalLAnchorPos; // Create vector for original anchor positions
+        Vector3 newRPos = originalRAnchorPos;
+
+        newLPos.x -= anchorOffset * cardCount; // Set the new X positions to be the offset * the card count
+        newRPos.x += anchorOffset * cardCount;
+
+        leftAnchor.position = newLPos; // Set our anchors to these new positions
+        rightAnchor.position = newRPos;
     }
 }
